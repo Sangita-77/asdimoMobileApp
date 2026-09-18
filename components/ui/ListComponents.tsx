a8d351a35741b3c7f1590b1e7e74d6c92d5d0dd0
swati-bazal <swati@dreamlogodesign.net>
2026-09-18 19:20:01 +0530

    Appointment List Fixed CSS

--- components/ui/ListComponents.tsx
+++ components/ui/ListComponents.tsx
@@ -92,6 +92,7 @@ export default function DoctorListCard({
         </View>
 
         <View style={styles.details}>
+          <Text style={styles.experience}><span style={styles.experienceSpan}>20+ Years</span> Experience</Text>
           <Text style={styles.name}>{name}</Text>
           <Text style={styles.specialty}>Therapist</Text>
 
@@ -150,9 +151,11 @@ export default function DoctorListCard({
 }
 
 const styles = StyleSheet.create({
+  experienceSpan:{fontSize: 15, fontWeight: "600"},
+  experience:{backgroundColor: "#abd8ff", width: 140, borderRadius: 4, color: "#054375", fontSize: 10, textAlign: "center", position: "absolute", right: 0, top: -23, padding: 2,},
   button: { backgroundColor: "#1386E7", paddingHorizontal: 8, paddingVertical: 4, borderRadius: 4, }, 
   buttonText: { color: "#FFF", fontWeight: "600", fontSize: 11, lineHeight: 14,},
-  card: { backgroundColor: "#FFF", borderRadius: 15, borderWidth: 0, borderColor: "#e7e6e6", padding: 12, marginVertical: 8, elevation: 4, boxShadow: "0px 6px 14px rgba(0, 0, 0, 0.1)", },
+  card: { backgroundColor: "#FFF", borderRadius: 15, borderWidth: 0, borderColor: "#e7e6e6", padding: 12, marginVertical: 15, elevation: 4, boxShadow: "0px 6px 14px rgba(0, 0, 0, 0.44)", },
   topSection: { flexDirection: "row", },
   image: { width: 70, height: 70, borderRadius: 10 },
   avatarPlaceholder: { width: 95, height: 95, borderRadius: 10, backgroundColor: "#1682E7", justifyContent: "center", alignItems: "center", },
